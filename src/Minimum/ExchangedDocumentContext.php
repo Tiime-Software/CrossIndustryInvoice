f16345caf58bc49c9d7905d3dbc2a76256beebc0
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-07 11:37:24 +0200

    remove GuidelineSpecifiedDocumentContextParameter from constructor

--- src/Minimum/ExchangedDocumentContext.php
+++ src/Minimum/ExchangedDocumentContext.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tiime\CrossIndustryInvoice\Minimum;
 
+use Tiime\EN16931\DataType\Identifier\SpecificationIdentifier;
+
 class ExchangedDocumentContext
 {
     /**
@@ -16,20 +18,15 @@ class ExchangedDocumentContext
      */
     private GuidelineSpecifiedDocumentContextParameter $guidelineSpecifiedDocumentContextParameter;
 
-    public function __construct(
-        GuidelineSpecifiedDocumentContextParameter $guidelineSpecifiedDocumentContextParameter
-    ) {
-        $this->guidelineSpecifiedDocumentContextParameter = $guidelineSpecifiedDocumentContextParameter;
+    public function __construct() {
+        $this->guidelineSpecifiedDocumentContextParameter = new GuidelineSpecifiedDocumentContextParameter(
+            new SpecificationIdentifier(SpecificationIdentifier::MINIMUM)
+        );
     }
 
     public function getBusinessProcessSpecifiedDocumentContextParameter(): ?BusinessProcessSpecifiedDocumentContextParameter
     {
         return $this->businessProcessSpecifiedDocumentContextParameter;
-    }
-
-    public function setBusinessProcessSpecifiedDocumentContextParameter(?BusinessProcessSpecifiedDocumentContextParameter $businessProcessSpecifiedDocumentContextParameter): void
-    {
-        $this->businessProcessSpecifiedDocumentContextParameter = $businessProcessSpecifiedDocumentContextParameter;
     }
 
     public function getGuidelineSpecifiedDocumentContextParameter(): GuidelineSpecifiedDocumentContextParameter
