f23457e40a7de39e890b283febd51468d9b8d3d1
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-13 16:15:53 +0200

    add ApplicableHeaderTradeDelivery classes

--- src/EN16931/ActualDeliverySupplyChainEvent.php
+++ src/EN16931/ActualDeliverySupplyChainEvent.php
@@ -9,4 +9,18 @@ namespace Tiime\CrossIndustryInvoice\EN16931;
  */
 class ActualDeliverySupplyChainEvent
 {
+    /**
+     * BT-72-01.
+     */
+    private OccurenceDateTime $occurenceDateTime;
+
+    public function __construct(OccurenceDateTime $occurenceDateTime)
+    {
+        $this->occurenceDateTime = $occurenceDateTime;
+    }
+
+    public function getOccurenceDateTime(): OccurenceDateTime
+    {
+        return $this->occurenceDateTime;
+    }
 }
--- src/EN16931/ReceivingAdviceReferencedDocument.php
+++ src/EN16931/ReceivingAdviceReferencedDocument.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Tiime\CrossIndustryInvoice\EN16931;
 
+use Tiime\EN16931\DataType\Reference\ReceivingAdviceReference;
+
 /**
  * BT-15-00.
  */
 class ReceivingAdviceReferencedDocument
 {
+    /**
+     * BT-15.
+     */
+    private ReceivingAdviceReference $issuerAssignedID;
+
+    public function __construct(ReceivingAdviceReference $issuerAssignedID)
+    {
+        $this->issuerAssignedID = $issuerAssignedID;
+    }
+
+    public function getIssuerAssignedID(): ReceivingAdviceReference
+    {
+        return $this->issuerAssignedID;
+    }
 }
