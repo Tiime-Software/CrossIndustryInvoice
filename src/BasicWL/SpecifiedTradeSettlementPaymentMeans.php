ceb30145a29d0cf3af19cf5938097e4fc5f777fe
Lucas-Gerard <lucas.gerard.web@gmail.com>
2023-04-17 13:46:09 +0200

    Move PayerPartyDebtorFinancialAccount to DataTypes

--- src/BasicWL/SpecifiedTradeSettlementPaymentMeans.php
+++ src/BasicWL/SpecifiedTradeSettlementPaymentMeans.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tiime\CrossIndustryInvoice\BasicWL;
 
+use Tiime\CrossIndustryInvoice\DataType\PayerPartyDebtorFinancialAccount;
 use Tiime\EN16931\DataType\PaymentMeansCode;
 
 /**
