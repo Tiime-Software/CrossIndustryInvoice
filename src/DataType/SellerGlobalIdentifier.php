1fa319798dcc800654333956a61806b821a3d014
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-19 09:59:26 +0200

    improve SellerGlobalIdentifier

---
+++ src/DataType/SellerGlobalIdentifier.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tiime\CrossIndustryInvoice\DataType;
+
+use Tiime\EN16931\DataType\Identifier\SellerIdentifier;
+use Tiime\EN16931\DataType\InternationalCodeDesignator;
+
+/**
+ * BT-29-0 & BT-29-1.
+ */
+class SellerGlobalIdentifier extends SellerIdentifier
+{
+    public function __construct(string $value, InternationalCodeDesignator $scheme)
+    {
+        parent::__construct($value, $scheme);
+    }
+}
