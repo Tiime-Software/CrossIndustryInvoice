9c1a722e7b5a3a177105857f79da5dd8eab6fba8
Lucas-Gerard <lucas.gerard.web@gmail.com>
2023-04-25 10:42:53 +0200

    Add properties to NetPriceProductTradePrice

--- src/Basic/NetPriceProductTradePrice.php
+++ src/Basic/NetPriceProductTradePrice.php
@@ -2,7 +2,11 @@
 
 namespace Tiime\CrossIndustryInvoice\Basic;
 
+use Tiime\CrossIndustryInvoice\EN16931\NetPriceProductTradePrice\BasisQuantity;
+use Tiime\EN16931\SemanticDataType\UnitPriceAmount;
+
 class NetPriceProductTradePrice
 {
-
+    private UnitPriceAmount $unitPriceAmount;
+    private ?BasisQuantity $basisQuantity;
 }
