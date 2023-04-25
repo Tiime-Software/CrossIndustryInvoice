eaf2106f61c5155d541b8c528a1ecc8d7fc4fb34
Lucas-Gerard <lucas.gerard.web@gmail.com>
2023-04-25 10:58:01 +0200

    Add properties to SpecifiedTradeAllowance & Charge

--- src/Basic/SpecifiedTradeAllowance.php
+++ src/Basic/SpecifiedTradeAllowance.php
@@ -2,7 +2,14 @@
 
 namespace Tiime\CrossIndustryInvoice\Basic;
 
+use Tiime\CrossIndustryInvoice\DataType\AllowanceChargeIndicator;
+use Tiime\EN16931\DataType\AllowanceReasonCode;
+use Tiime\EN16931\SemanticDataType\Amount;
+
 class SpecifiedTradeAllowance
 {
-
+    private AllowanceChargeIndicator $chargeIndicator;
+    private Amount $actualAmount;
+    private ?AllowanceReasonCode $reasonCode;
+    private ?string $reason;
 }
