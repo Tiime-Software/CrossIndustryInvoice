51e6e60aef9dc160aeb68470aced2e2a9b4c79a6
Lucas-Gerard <lucas.gerard.web@gmail.com>
2023-04-25 10:45:14 +0200

    Add property to SpecifiedLineTradeDelivery

--- src/Basic/SpecifiedLineTradeDelivery.php
+++ src/Basic/SpecifiedLineTradeDelivery.php
@@ -2,7 +2,9 @@
 
 namespace Tiime\CrossIndustryInvoice\Basic;
 
+use Tiime\CrossIndustryInvoice\EN16931\BilledQuantity;
+
 class SpecifiedLineTradeDelivery
 {
-
+    private BilledQuantity $billedQuantity;
 }
