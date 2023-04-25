547e7376116a366c74d35cc95b66957e1dac5904
Lucas-Gerard <lucas.gerard.web@gmail.com>
2023-04-25 10:35:48 +0200

    Add SpecifiedTradeProduct properties

--- src/Basic/SpecifiedTradeProduct.php
+++ src/Basic/SpecifiedTradeProduct.php
@@ -2,7 +2,10 @@
 
 namespace Tiime\CrossIndustryInvoice\Basic;
 
+use Tiime\EN16931\DataType\Identifier\StandardItemIdentifier;
+
 class SpecifiedTradeProduct
 {
-
+    private ?StandardItemIdentifier $globalIdentifier;
+    private string $name;
 }
