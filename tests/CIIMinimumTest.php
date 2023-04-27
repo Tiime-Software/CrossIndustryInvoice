405f1331d9a1f3ce44d8ce78581838e9972f25af
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-27 14:51:29 +0200

    improve test Minimum profile

--- tests/CIIMinimumTest.php
+++ tests/CIIMinimumTest.php
@@ -105,13 +105,17 @@ class CIIMinimumTest extends TestCase
         $vatTaxId1 = new SpecifiedTaxRegistration(new VatIdentifier('FRVatId1'));
         $vatTaxId2 = new SpecifiedTaxRegistration(new VatIdentifier('FRVatId2'));
         $sellerTradeParty->setSpecifiedTaxRegistrations([$vatTaxId1, $vatTaxId2]);
+        $sellerTradeParty->setSpecifiedLegalOrganization(
+            (new SellerSpecifiedLegalOrganization())
+                ->setIdentifier(new LegalRegistrationIdentifier('SellerLegalRegistrationIdentifier', InternationalCodeDesignator::FRANCE_TELECOM_ATM_END_SYSTEM_ADDRESS_PLAN))
+        );
 
         /** SpecifiedTradeSettlementHeaderMonetarySummation part */
         $specifiedTradeSettlementHeaderMonetarySummation = new SpecifiedTradeSettlementHeaderMonetarySummation(
             12,
             13,
             14,
-            new TaxTotalAmount('24', CurrencyCode::EURO)
+            new TaxTotalAmount('24.5', CurrencyCode::EURO)
         );
 
         $invoice = new CrossIndustryInvoice(
@@ -142,11 +146,11 @@ class CIIMinimumTest extends TestCase
 
     /**
      * @test
-     * @testdox Create Minimum profile from XML data
+     * @testdox Create Minimum profile from XML mandatory data
      */
-    public function createMinimumProfileFromXMLData(): void
+    public function createMinimumProfileFromXMLMandatoryData(): void
     {
-        $invoice = new CrossIndustryInvoice(
+        $invoiceToConvert = new CrossIndustryInvoice(
             new ExchangedDocumentContext(
                 new GuidelineSpecifiedDocumentContextParameter(
                     new SpecificationIdentifier(SpecificationIdentifier::MINIMUM)
@@ -170,11 +174,90 @@ class CIIMinimumTest extends TestCase
             )
         );
 
-        $xmlInvoice = $invoice->toXML();
+        $xmlInvoice = $invoiceToConvert->toXML();
 
         $document = new \DOMDocument();
         $document->loadXML($xmlInvoice->saveXML());
 
-        CrossIndustryInvoice::fromXML($document);
+        $invoice = CrossIndustryInvoice::fromXML($document);
+        $this->assertInstanceOf(CrossIndustryInvoice::class, $invoice);
+    }
+
+    /**
+     * @test
+     * @testdox Create Minimum profile from XML mandatory and optional data
+     */
+    public function createMinimumProfileFromXMLMandatoryAndOptionalData(): void
+    {
+        /** ExchangedDocumentContext part */
+        $exchangedDocumentContext = new ExchangedDocumentContext(
+            new GuidelineSpecifiedDocumentContextParameter(
+                new SpecificationIdentifier(SpecificationIdentifier::MINIMUM)
+            )
+        );
+        $businessProcessSpecifiedDocumentContextParameter = new BusinessProcessSpecifiedDocumentContextParameter('BusinessProcess1');
+        $exchangedDocumentContext->setBusinessProcessSpecifiedDocumentContextParameter($businessProcessSpecifiedDocumentContextParameter);
+
+        /** BuyerTradeParty part */
+        $buyerTradeParty                 = new BuyerTradeParty('BuyerTradePartyName');
+        $buyerSpecifiedLegalOrganization = new BuyerSpecifiedLegalOrganization(
+            new LegalRegistrationIdentifier(
+                'LegalRegistrationBuyer',
+                InternationalCodeDesignator::FRANCE_TELECOM_ATM_END_SYSTEM_ADDRESS_PLAN
+            )
+        );
+        $buyerTradeParty->setSpecifiedLegalOrganization($buyerSpecifiedLegalOrganization);
+
+        /** SellerTradeParty part */
+        $sellerTradeParty                 = new SellerTradeParty('SellerTradePartyName', new PostalTradeAddress(CountryAlpha2Code::FRANCE));
+        $sellerSpecifiedLegalOrganization = new SellerSpecifiedLegalOrganization();
+        $legalRegistrationIdentifier      = new LegalRegistrationIdentifier(
+            'LegalRegistrationSeller',
+            InternationalCodeDesignator::FRANCE_TELECOM_ATM_END_SYSTEM_ADDRESS_PLAN
+        );
+        $sellerSpecifiedLegalOrganization->setIdentifier($legalRegistrationIdentifier);
+        $vatTaxId1 = new SpecifiedTaxRegistration(new VatIdentifier('FRVatId1'));
+        $vatTaxId2 = new SpecifiedTaxRegistration(new VatIdentifier('FRVatId2'));
+        $sellerTradeParty->setSpecifiedTaxRegistrations([$vatTaxId1, $vatTaxId2]);
+        $sellerTradeParty->setSpecifiedLegalOrganization(
+            (new SellerSpecifiedLegalOrganization())
+                ->setIdentifier(new LegalRegistrationIdentifier('SellerLegalRegistrationIdentifier', InternationalCodeDesignator::FRANCE_TELECOM_ATM_END_SYSTEM_ADDRESS_PLAN))
+        );
+
+        /** SpecifiedTradeSettlementHeaderMonetarySummation part */
+        $specifiedTradeSettlementHeaderMonetarySummation = new SpecifiedTradeSettlementHeaderMonetarySummation(
+            12,
+            13,
+            14,
+            new TaxTotalAmount('24.5', CurrencyCode::EURO)
+        );
+
+        $invoiceToConvert = new CrossIndustryInvoice(
+            $exchangedDocumentContext,
+            new ExchangedDocument(
+                new InvoiceIdentifier('FA-1545'),
+                InvoiceTypeCode::COMMERCIAL_INVOICE,
+                new IssueDateTime(new \DateTime())
+            ),
+            new SupplyChainTradeTransaction(
+                new ApplicableHeaderTradeAgreement(
+                    $sellerTradeParty,
+                    $buyerTradeParty,
+                    new BuyerOrderReferencedDocument(new PurchaseOrderReference('A380'))
+                ),
+                new ApplicableHeaderTradeSettlement(
+                    CurrencyCode::EURO,
+                    $specifiedTradeSettlementHeaderMonetarySummation
+                )
+            )
+        );
+
+        $xmlInvoice = $invoiceToConvert->toXML();
+
+        $document = new \DOMDocument();
+        $document->loadXML($xmlInvoice->saveXML());
+
+        $invoice = CrossIndustryInvoice::fromXML($document);
+        $this->assertInstanceOf(CrossIndustryInvoice::class, $invoice);
     }
 }
