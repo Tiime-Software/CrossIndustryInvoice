537d763f267041a0f84cd8794a4d3ceae83ddf81
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-28 17:11:44 +0200

    add fromXML for BasicWL LocationGlobalIdentifier

--- src/DataType/LocationGlobalIdentifier.php
+++ src/DataType/LocationGlobalIdentifier.php
@@ -12,8 +12,36 @@ use Tiime\EN16931\DataType\InternationalCodeDesignator;
  */
 class LocationGlobalIdentifier extends LocationIdentifier
 {
+    protected const XML_NODE = 'ram:GlobalID';
+
     public function __construct(string $value, InternationalCodeDesignator $scheme)
     {
         parent::__construct($value, $scheme);
+    }
+
+    public static function fromXML(\DOMXPath $xpath, \DOMElement $currentElement): ?static
+    {
+        $locationGlobalIdentifierElements = $xpath->query(sprintf('.//%s', self::XML_NODE), $currentElement);
+
+        if (0 === $locationGlobalIdentifierElements->count()) {
+            return null;
+        }
+
+        if ($locationGlobalIdentifierElements->count() > 1) {
+            throw new \Exception('Malformed');
+        }
+
+        /** @var \DOMElement $locationGlobalIdentifierElement */
+        $locationGlobalIdentifierElement = $locationGlobalIdentifierElements->item(0);
+
+        $identifier = $locationGlobalIdentifierElement->nodeValue;
+        $scheme     = '' !== $locationGlobalIdentifierElement->getAttribute('schemeID') ?
+            InternationalCodeDesignator::tryFrom($locationGlobalIdentifierElement->getAttribute('schemeID')) : null;
+
+        if (null === $scheme) {
+            throw new \Exception('Wrong schemeID');
+        }
+
+        return new static($identifier, $scheme);
     }
 }
