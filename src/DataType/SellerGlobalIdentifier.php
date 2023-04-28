3398f256f441786c2217edb5beb5427896e1a795
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-28 15:47:51 +0200

    add fromXML for BasicWL SellerGlobalIdentifier

--- src/DataType/SellerGlobalIdentifier.php
+++ src/DataType/SellerGlobalIdentifier.php
@@ -12,6 +12,8 @@ use Tiime\EN16931\DataType\InternationalCodeDesignator;
  */
 class SellerGlobalIdentifier extends SellerIdentifier
 {
+    protected const XML_NODE = 'ram:GlobalID';
+
     public function __construct(string $value, InternationalCodeDesignator $scheme)
     {
         parent::__construct($value, $scheme);
@@ -24,6 +26,26 @@ class SellerGlobalIdentifier extends SellerIdentifier
 
     public static function fromXML(\DOMXPath $xpath, \DOMElement $currentElement): array
     {
-        // todo
+        $sellerGlobalIdentifierElements = $xpath->query(sprintf('.//%s', self::XML_NODE), $currentElement);
+
+        if (0 === $sellerGlobalIdentifierElements->count()) {
+            return [];
+        }
+
+        $sellerGlobalIdentifiers = [];
+
+        foreach ($sellerGlobalIdentifierElements as $sellerGlobalIdentifierElement) {
+            $sellerGlobalIdentifier = $sellerGlobalIdentifierElement->nodeValue;
+            $scheme                 = '' !== $sellerGlobalIdentifierElement->getAttribute('schemeID') ?
+                InternationalCodeDesignator::tryFrom($sellerGlobalIdentifierElement->getAttribute('schemeID')) : null;
+
+            if (null === $scheme) {
+                throw new \Exception('Wrong schemeID');
+            }
+
+            $sellerGlobalIdentifiers[] = new self($sellerGlobalIdentifier, $scheme);
+        }
+
+        return $sellerGlobalIdentifiers;
     }
 }
