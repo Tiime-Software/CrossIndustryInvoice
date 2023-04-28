83bb9358515f3f8c82b8d12d3b5f526689903642
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2023-04-28 16:18:13 +0200

    add fromXML for BasicWL OccurrenceDateTime

--- src/DataType/OccurrenceDateTime.php
+++ src/DataType/OccurrenceDateTime.php
@@ -6,6 +6,8 @@ namespace Tiime\CrossIndustryInvoice\DataType;
 
 class OccurrenceDateTime
 {
+    protected const XML_NODE = 'ram:OccurrenceDateTime';
+
     /**
      * BT-72.
      */
@@ -34,13 +36,48 @@ class OccurrenceDateTime
 
     public function toXML(\DOMDocument $document): \DOMElement
     {
-        $element = $document->createElement('ram:OccurrenceDateTime');
+        $element = $document->createElement(self::XML_NODE);
 
-        $dateTimeElement = $document->createElement('ram:DateTimeString', $this->dateTimeString->format('Ymd'));
+        $dateTimeElement = $document->createElement('udt:DateTimeString', $this->dateTimeString->format('Ymd'));
         $dateTimeElement->setAttribute('format', $this->format);
 
         $element->appendChild($dateTimeElement);
 
         return $element;
+    }
+
+    public static function fromXML(\DOMXPath $xpath, \DOMElement $currentElement): static
+    {
+        $occurrenceDateTimeElements = $xpath->query(sprintf('.//%s', self::XML_NODE), $currentElement);
+
+        if (1 !== $occurrenceDateTimeElements->count()) {
+            throw new \Exception('Malformed');
+        }
+
+        /** @var \DOMElement $occurrenceDateTimeElement */
+        $occurrenceDateTimeElement = $occurrenceDateTimeElements->item(0);
+
+        $dateTimeStringElements = $xpath->query('.//udt:DateTimeString', $occurrenceDateTimeElement);
+
+        if (1 !== $dateTimeStringElements->count()) {
+            throw new \Exception('Malformed');
+        }
+
+        $dateTimeStringItem = $dateTimeStringElements->item(0);
+        $dateTimeString     = $dateTimeStringItem->nodeValue;
+
+        if ('102' !== $dateTimeStringItem->getAttribute('format')) {
+            throw new \Exception('Wrong format');
+        }
+
+        $formattedDateTime = \DateTime::createFromFormat('Ymd', $dateTimeString);
+
+        if (!$formattedDateTime) {
+            throw new \Exception('Malformed date');
+        }
+
+        $formattedDateTime->setTime(0, 0);
+
+        return new static($formattedDateTime);
     }
 }
