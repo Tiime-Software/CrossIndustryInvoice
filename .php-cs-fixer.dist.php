0451b52a91602c1d0f13078394ade7f7aa8bd9e4
Aurélien Pillevesse <aurelienpillevesse@hotmail.fr>
2024-11-19 11:22:56 +0100

    update deps and cs fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,18 +2,18 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in([
-        __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR,
-        __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR,
+        __DIR__ . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR,
+        __DIR__ . \DIRECTORY_SEPARATOR . 'tests' . \DIRECTORY_SEPARATOR,
     ])
 ;
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules(
         [
             '@Symfony'                              => true,
             '@Symfony:risky'                        => true,
             'no_unreachable_default_argument_value' => false,
-            'braces'                                => ['allow_single_line_closure' => true],
             'heredoc_to_nowdoc'                     => false,
             'phpdoc_annotation_without_dot'         => false,
             'strict_comparison'                     => true,
@@ -38,15 +38,17 @@ return (new PhpCsFixer\Config())
                     'break',
                 ],
             ],
-            'yoda_style'                             => true,
-            'no_superfluous_phpdoc_tags'             => true,
-            'combine_consecutive_issets'             => true,
-            'combine_consecutive_unsets'             => true,
-            'method_chaining_indentation'            => true,
-            'php_unit_test_case_static_method_calls' => ['call_type' => 'this'],
-            'single_line_throw'                      => false,
-            'mb_str_functions'                       => true,
-            'nullable_type_declaration_for_default_null_value' => true
+            'yoda_style'                                       => true,
+            'no_superfluous_phpdoc_tags'                       => true,
+            'combine_consecutive_issets'                       => true,
+            'combine_consecutive_unsets'                       => true,
+            'method_chaining_indentation'                      => true,
+            'php_unit_test_case_static_method_calls'           => ['call_type' => 'this'],
+            'single_line_throw'                                => false,
+            'mb_str_functions'                                 => true,
+            'nullable_type_declaration_for_default_null_value' => true,
+            'protected_to_private'                             => false,
+            'trailing_comma_in_multiline'                      => ['elements' => ['arrays', 'match', 'parameters']],
         ]
     )
     ->setRiskyAllowed(true)
